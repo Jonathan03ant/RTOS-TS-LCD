d26a2e60e8780b346dc2ad73effc53047239c598
jonathan03ant <jonathan03ant@gmail.com>
2024-09-27 13:29:52 -0400

    Task handle def

--- lcd_rtos.c
+++ lcd_rtos.c
@@ -10,6 +10,15 @@
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
+/*	
+	* The following definitions handle different tasks
+	* We can add as many tasks as we need
+*/
+
+TaskHandle_t TimeDisplayTaskHandle;
+TaskHandle_t statusDisplayTaskHandle;
+TaskHandle_t GoodByeTaskHandle;
+
 
 void setup() {
 
